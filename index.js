c53f3d3eae5366891b1e8d5b5e8415ba0967cf55
GRIFFIN <griffin@ciluffo.com>
2017-11-16 19:44:27 -0700

    dcp it up

--- index.js
+++ index.js
@@ -2,21 +2,38 @@ const ChangesStream = require('changes-stream');
 const Request = require('request');
 const Normalize = require('normalize-registry-metadata');
 
-const db = 'https://replicate.npmjs.com';
+const db = 'http://172.17.0.1:5984/registry';
 
 var changes = new ChangesStream({
   db: db,
   include_docs: true
 });
 
-Request.get(db, function(err, req, body) {
-  var end_sequence = JSON.parse(body).update_seq;
-  changes.on('data', function(change) {
-    if (change.seq >= end_sequence) {
-      process.exit(0);
-    }
-    if (change.doc.name) {
-      console.log(Normalize(change.doc));
-    }
-  });
+changes.on('data', function(change) {
+  if (change.doc.name && happenedRecently(change.doc)) {
+    postToSlack(change.doc);
+  }
 });
+
+function happenedRecently(doc) {
+  var timeStamp = new Date(doc.time['modified']);
+  var diff = new Date() - timeStamp;
+  return diff < 6000;
+}
+
+function postToSlack(doc) {
+    console.log('time to post', doc.name);
+    Request({
+      method: 'POST',
+      url: 'https://hooks.slack.com/services/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',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: {
+        "text": `${doc.name} updated to ${doc['dist-tags']['latest']}`
+      },
+      json: true
+    }, function (error, response, body) {
+
+    });
+}
